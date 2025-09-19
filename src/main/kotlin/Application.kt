ecfb5538829598d315dd29fd370d2686a8db0b14
Yurii Sika <yrasika80@gmail.com>
2025-09-19 20:51:57 +0100

    config

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,8 +1,11 @@
 import io.ktor.server.application.*
-import io.ktor.server.netty.EngineMain
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
-fun main(args: Array<String>) {
-    EngineMain.main(args)
+fun main() {
+    embeddedServer(Netty, port = System.getenv("PORT")?.toInt() ?: 8080) {
+        module()
+    }.start(wait = true)
 }
 
 fun Application.module() {
